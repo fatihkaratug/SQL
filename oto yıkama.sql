8b4e4e93419fab3bea3c6c4822535c50f5a1c29c
fatihkaratug <law.fkaratug32@gmail.com>
2022-11-06 07:16:37 +0300

    sql

---
+++ oto yıkama.sql
@@ -0,0 +1,10 @@
+create table oto_yikama
+(
+isim varchar(10),
+araba varchar(20),
+ucret int,
+tarih date
+);
+insert into oto_yikama values('emil', ('duster'), 90, '2022-11-05');
+insert into oto_yikama values('emil', ('i20'), 80, '2022-11-05');
+select*from oto_yikama
